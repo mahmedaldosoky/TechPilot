be94836e29844d845e6f9e37bce2bf122138e7de
mahmedaldosoky <mahmedaldosoky@gmail.com>
2024-01-11 14:30:46 +0200

    https api

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl:'http://techpilot.somee.com/api',
+  apiUrl:'https://techpilot.somee.com/api',
   // apiUrl: 'https://localhost:7229/api',
 };
